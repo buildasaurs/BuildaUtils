cf1eb44d4549aaa5b42636de15b8bd9dd7e99c53
Honza Dvorsky <honza@swiftkey.com>
2015-07-18 14:16:30 +0100

    adding files

---
+++ BuildaUtils/BuildaUtils.h
@@ -0,0 +1,19 @@
+//
+//  BuildaUtils.h
+//  BuildaUtils
+//
+//  Created by Honza Dvorsky on 18/07/2015.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BuildaUtils.
+FOUNDATION_EXPORT double BuildaUtilsVersionNumber;
+
+//! Project version string for BuildaUtils.
+FOUNDATION_EXPORT const unsigned char BuildaUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildaUtils/PublicHeader.h>
+
+
