6e55cc615cddc25565f3a81fc87fccdafdac37fc
Honza Dvorsky <honza@swiftkey.com>
2015-08-10 09:24:49 +0100

    added a watchOS target so that Carthage users can use BuildaUtils (in XcodeServerSDK) on the watch as well

---
+++ BuildaUtilswatchOS/BuildaUtilswatchOS.h
@@ -0,0 +1,19 @@
+//
+//  BuildaUtilswatchOS.h
+//  BuildaUtilswatchOS
+//
+//  Created by Honza Dvorsky on 10/08/2015.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BuildaUtilswatchOS.
+FOUNDATION_EXPORT double BuildaUtilswatchOSVersionNumber;
+
+//! Project version string for BuildaUtilswatchOS.
+FOUNDATION_EXPORT const unsigned char BuildaUtilswatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildaUtilswatchOS/PublicHeader.h>
+
+
