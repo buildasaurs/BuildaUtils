8ead57855ebc834b3f0782840ff22f95efa30be7
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-10-03 13:06:48 +0100

    ref++

---
+++ BuildaUtils/Ref.swift
@@ -0,0 +1,18 @@
+//
+//  Ref.swift
+//  BuildaUtils
+//
+//  Created by Honza Dvorsky on 10/3/15.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+import Foundation
+
+//for serialization of object references
+public typealias RefType = String
+
+public struct Ref {
+    static func new() -> RefType {
+        return NSUUID().UUIDString
+    }
+}
